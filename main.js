69505f427586879f5eb01c9ffd1b1c664652a249
Mou4d <bomou4d@gmail.com>
2020-09-20 01:16:31 +0100

    Creat needed files and app body

---
+++ main.js
@@ -0,0 +1,19 @@
+new Vue({
+    'el': '#app',
+    'data': {
+        newTask:"",
+        tasks:[
+            {text:"Task1", completed:false},
+            {text:"Task2", completed:false},
+            {text:"Task3", completed:true},
+        ]
+    },
+    'methods':{
+        addNewTask: function (){
+            if(this.newTask === "")
+                return;
+            this.tasks.push({text:this.newTask, completed: false});
+            newTask = "";
+        }
+    }
+});
