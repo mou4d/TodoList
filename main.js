40c76c7b2da59f412b53ad18e953622a160de827
Mou4d <bomou4d@gmail.com>
2020-10-04 01:11:05 +0100

    Some bug fixes

--- main.js
+++ main.js
@@ -18,6 +18,8 @@ new Vue({
     'data': {
         filter: "all",
         newTask:"",
+        oldTask: null,
+        updatedTask: null,
         tasks:[
             {text:"Task1", completed:false},
             {text:"Task2", completed:false},
@@ -55,6 +57,24 @@ new Vue({
         },
         deleteCompleteTasks: function (){
             this.tasks = filters.active(this.tasks);
+        },
+        startEditing(task, evt){
+            if(this.updatedTask == null){
+                var input = evt.target.parentElement.children[2];
+                evt.target.classList.add("hide");
+                input.classList.add("show");
+                input.focus();
+                this.updatedTask = task.text;
+                this.oldTask = task;
+            } 
+        },
+        endEditig(evt){
+            if(this.updatedTask !== ""){
+                this.oldTask.text = this.updatedTask;
+                evt.target.classList.remove("show");
+                evt.target.parentElement.children[1].classList.add("show");
+                this.updatedTask = null;
+            }
         }
     }
 });
